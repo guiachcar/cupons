66b9fbf33cde67bfa306416b1eaf12401c3fa983
Guilherme <guiachcar@hotmail.com>
2018-07-10 15:42:06 -0300

    acerto de banco

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/gstest?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@178.128.148.90:3306/gstest?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
