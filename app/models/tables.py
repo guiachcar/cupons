4555f34e67ede09a42bc5b64f4a5ca2537bd0669
Guilherme <guiachcar@hotmail.com>
2018-07-11 22:40:32 -0300

    configuracao id_compra

--- app/models/tables.py
+++ app/models/tables.py
@@ -4,13 +4,15 @@ class Cupom(db.Model):
 	__tablename__ = 'cupoms'
 	_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	nome = db.Column(db.String(100))
-	email = db.Column(db.String(100), unique=True)
+	email = db.Column(db.String(100))
 	valor = db.Column(db.Float())
+	id_compra = db.Column(db.Integer(), nullable=True)
     
-	def __init__(self, nome, email, valor):
+	def __init__(self, nome, email, valor, id_compra):
 		self.nome = nome
 		self.email = email
 		self.valor = valor
+		self.id_compra = id_compra
 	def save(self):
 		db.session.add(self)
 		return db.session.commit()
